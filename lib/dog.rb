33458a08abe12f850ff601996b0d7a3300df30e1
Milos Popovic <milos_popovic@outlook.com>
2020-05-24 20:57:01 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+  def talk
+    puts "Hello World!"
+  end
+end
